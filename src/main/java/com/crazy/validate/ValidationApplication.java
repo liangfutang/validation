34db0e110991214b65697f985e2b4614ce69b8a7
love向天笑 <tangliangfucom@sina.com>
2019-06-24 23:50:20 +0800

    测试用

--- src/main/java/com/crazy/validate/ValidationApplication.java
+++ src/main/java/com/crazy/validate/ValidationApplication.java
@@ -1,7 +1,26 @@
 package com.crazy.validate;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 public class ValidationApplication {
     public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>());
+        executor.prestartAllCoreThreads();
+        executor.execute(() -> {
+            System.out.println("子线程开始");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("子线程结束");
+        });
+        executor.shutdown();
         System.out.println("测试");
     }
 }
